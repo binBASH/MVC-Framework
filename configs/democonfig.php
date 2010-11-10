e465bd324858d582c122f687aac5c3e65521b3e5
Michael Maier <maier@b2host.de>
2010-11-10 17:43:57 +0100

    Initial ocmmit

---
+++ configs/democonfig.php
@@ -0,0 +1,10 @@
+<?php
+final class Config
+{
+    const db_driver = 'mysqli';
+    const db_host = '10.10.0.1';
+    const db_username = 'testuser';
+    const db_password = 'testpw';
+    const db_name = 'testdb';
+}
+?>
